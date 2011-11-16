7a9c37a1461f978bd9d520be6719ac6c130fcb8e
Cantoraz Zhou <cantoraz@gmail.com>
2011-11-16 22:22:14 +0800

    Add Log and ApplicationContextAware support.

--- src/test/java/cn/caz/elen/core/AbstractTest.java
+++ src/test/java/cn/caz/elen/core/AbstractTest.java
@@ -3,7 +3,12 @@
  */
 package cn.caz.elen.core;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.junit.runner.RunWith;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -13,6 +18,23 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath:applicationContext.xml" })
-public abstract class AbstractTest {
+public abstract class AbstractTest implements ApplicationContextAware {
+
+	protected final Log logger = LogFactory.getLog(getClass());
+
+	protected ApplicationContext applicationContext;
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.springframework.context.ApplicationContextAware#setApplicationContext
+	 * (org.springframework.context.ApplicationContext)
+	 */
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext)
+			throws BeansException {
+		this.applicationContext = applicationContext;
+	}
 
 }
